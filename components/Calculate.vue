b4eae29a984fbe91b61455833f90c0f261af4756
Sarocha Boriraklerd <sarochaboriraklerd@gmail.com>
2021-06-24 22:34:42 +0700

    commit

---
+++ components/Calculate.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <div class="box">
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-btn
+            class="btn-cal"
+            color="red"
+            @click="$emit('Oncalculate')"
+          >
+            CALCULATE
+          </v-btn>
+        </v-col>
+        <v-col md="12">
+          <v-text-field
+            v-model="listJson"
+            class="answer"
+            label="ผลลัพธ์"
+            outlined
+            dense
+          />
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Calculate',
+  props: {
+    answer: {
+      type: Number,
+      default: Number
+    }
+  },
+  computed: {
+    listJson () {
+      return JSON.parse(this.answer)
+    }
+  }
+}
+</script>
+
+<style>
+.box{
+  padding-bottom: 10px;
+}
+
+.btn-cal {
+  position: relative;
+  top: -10px
+}
+
+.answer {
+  position: absolute;
+  left: 50px;
+}
+</style>
---
+++ pages/inputNumber.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container class="content">
+    <v-row>
+      <v-col cols="12" sm="6" md="6">
+        <v-text-field
+          v-model="input"
+          label="ข้อมูล"
+          outlined
+          dense
+        />
+      </v-col>
+      <v-col>
+        <Calculate :answer="result" @Oncalculate="calculate" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      input: '',
+      result: null
+    }
+  },
+  methods: {
+    calculate () {
+      const changeinput = Number(this.input)
+      this.result = changeinput / 100
+    }
+  }
+}
+</script>
+<style>
+.content {
+  position: relative;
+  background-color: #ffffff;
+  box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075);
+  border-radius: 3px;
+  padding: 50px;
+}
+</style>
